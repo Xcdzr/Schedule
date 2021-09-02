5d986a9e9adfa617727b5118a9e987d6e84a1e98
Kent Adams <79758004+Xcdzr@users.noreply.github.com>
2021-09-02 15:33:16 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+const day = new Date().getDay();
+switch(day){
+    case 0:
+        days="sunday";
+        dayss=6;
+        break;
+    case 1:
+        days="monday";
+        dayss=0;
+        break;
+    case 2:
+        days="tuesday";
+        dayss=1;
+        break;
+    case 3:
+        days="wednesday";
+        dayss=2;
+        break;
+    case 4:
+        days="thursday";
+        dayss=3;
+        break;
+    case 5:
+        days="friday";
+        dayss=4;
+        break;
+    case 6:
+        days="saturday";
+        dayss=5;
+        break;
+}
+document.getElementById(days).style.display = "block";
+const hour = new Date().getHours();
+switch(hour){
+    case 22:
+    case 23:
+    case 24:
+    case 0:
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+        hours="sleep";
+        document.getElementById(hours).style.display ="block";
+        break;
+    default:
+        hours="timeis"+hour;
+        document.getElementsByClassName(hours)[dayss].style.backgroundColor = "rgba(101,51,115,255)" ;
+        break;
+}
